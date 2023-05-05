d4da0b2f421c277a634499ba0ea769a0c9bb5807
Alejandro <alejandroshkn@gmail.com>
2023-05-05 17:53:45 -0500

    Se agregó el codigo que agrega un nuevo div con JS y se agrego su respectivo CSS

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -7,3 +7,27 @@ divPrincipal.appendChild(divNuevo);
 //   ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪ 
 
 //Codigo que agrega animaciones al HTML (opacidadad)
+let botonOcultarMotrar = document.getElementById(`ID-button__mostrarOcultar`);
+let divMostrarOcultar = document.getElementById(`ID-div__preloadContainerAnimacionJS`);
+
+botonOcultarMotrar.addEventListener(`click`, function(){
+    if(divMostrarOcultar.style.opacity == 0){
+        divMostrarOcultar.style.marginBottom = `20px`;
+        setTimeout(() => {
+            
+            divMostrarOcultar.style.opacity = 1;
+            divMostrarOcultar.style.width = `70%`;
+            divMostrarOcultar.style.height = `auto`;
+            divMostrarOcultar.style.marginTop = `20px`;
+            
+        }, 30);
+        
+    } else{
+        divMostrarOcultar.style.opacity = 0;
+        divMostrarOcultar.style.width = `30%`;
+        setTimeout(() => {
+            divMostrarOcultar.style.marginBottom = `-30px`;
+        }, 30);
+
+    }
+})
