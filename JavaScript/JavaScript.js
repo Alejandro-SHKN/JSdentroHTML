b58a89616eaee99b92f02aecc0dc24d6cb1cd9b7
Alejandro <alejandroshkn@gmail.com>
2023-05-07 02:03:43 -0500

    Se agregó una seccion donde se inicia sesion y se utilizó JS

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -50,22 +50,22 @@ nuevoDiv.classList.add(`div__nuevoDivcreado`, `transitionnuevoDivcreado`);
 botonOcultarMostrarNuevoDiv.addEventListener(`click`, function () {
   nuevoDiv.innerHTML = `<p class="p__parrafoDivNuevo transition">Este parrafo se creó con JavaScript desde 0 </p>`;
   if (nuevoDiv.style.opacity == 0) {
-    botonOcultarMostrarNuevoDiv.style.backgroundColor = `rgb(3, 255, 3)`
-    botonOcultarMostrarNuevoDiv.textContent = `On`
+    botonOcultarMostrarNuevoDiv.style.backgroundColor = `rgb(3, 255, 3)`;
+    botonOcultarMostrarNuevoDiv.textContent = `On`;
     nuevoDiv.style.backgroundColor = `white`;
     divPadreNuevoDiv.appendChild(nuevoDiv);
     setTimeout(() => {
-        nuevoDiv.style.margin = `10px`;
-    }, 100);
+      nuevoDiv.style.margin = `10px`;
+    }, 10);
     setTimeout(() => {
-        nuevoDiv.style.width = `90%`;
-        nuevoDiv.style.opacity = 1;
+      nuevoDiv.style.width = `90%`;
+      nuevoDiv.style.opacity = 1;
     }, 400);
     nuevoDiv.style.zIndex = `1`;
   } else {
-      botonOcultarMostrarNuevoDiv.textContent = `OFF`
-      botonOcultarMostrarNuevoDiv.style.backgroundColor = `red`
-      nuevoDiv.style.width = `45%`;
+    botonOcultarMostrarNuevoDiv.textContent = `OFF`;
+    botonOcultarMostrarNuevoDiv.style.backgroundColor = `red`;
+    nuevoDiv.style.width = `45%`;
     nuevoDiv.style.backgroundColor = `rgba(255, 255, 255, 0.311)`;
     nuevoDiv.style.zIndex = `-1`;
     setTimeout(() => {
@@ -73,5 +73,59 @@ botonOcultarMostrarNuevoDiv.addEventListener(`click`, function () {
       nuevoDiv.style.margin = `-20px`;
       nuevoDiv.style.opacity = 0;
     }, 500);
+  }
+});
+//   ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪  ⚪
+
+//Codigo que agrega un DIV con fotografias de los jugadores de F1
+
+let divIniciarSesion = document.getElementById(`ID-div__PadreInicioSesion`)
+let botonIniciarSesion = document.getElementById(`ID-button__iniciarSesion`);
+let divPadreInicioSesion = document.getElementById(`ID-div__PadrecontainerJugadoresF1`);
+
+let divJugadoresF1;
+
+botonIniciarSesion.addEventListener(`click`, function () {
+  let inputPassword = document.getElementById(`ID-input__Password`).value;
+  let inputNombre = document.getElementById(`ID-input__Nombre`).value;
+  let parrafoAlerta = document.createElement(`div`);
+  if (inputPassword === `abcd`) {
+    divIniciarSesion.remove();
+    divJugadoresF1 = document.createElement(`div`);
+    divJugadoresF1.classList.add(`div__containerJugadoresF1`);
+    divJugadoresF1.innerHTML = `<img src="./assets/imagenes__pilotosF1/1645221-max-verstappen.jpg" alt="" class="img__JugadorF1"><img src="./assets/imagenes__pilotosF1/ALO.jpg" alt="" class="img__JugadorF1"><img src="./assets/imagenes__pilotosF1/HAM.jpg" alt="" class="img__JugadorF1"><img src="./assets/imagenes__pilotosF1/Lec.jpg" alt="" class="img__JugadorF1"><img src="./assets/imagenes__pilotosF1/Perez.jpg" alt="" class="img__JugadorF1"><img src="./assets/imagenes__pilotosF1/RUS.jpg" alt="" class="img__JugadorF1"><img src="./assets/imagenes__pilotosF1/SAI.jpg" alt="" class="img__JugadorF1"><img src="./assets/imagenes__pilotosF1/STR.jpg" alt="" class="img__JugadorF1">`
+    divPadreInicioSesion.appendChild(divJugadoresF1)
+    parrafoAlerta.style.marginBottom = `60px`
+  } 
+   else if (inputNombre === `` ){
+    parrafoAlerta.classList.add(`div__ErrorAlert`,`transition`);
+    parrafoAlerta.innerHTML = `<p class="p__ErrorAlert">¡Error, introduce un nombre!</p>`;
+    divIniciarSesion.appendChild(parrafoAlerta)
+    setTimeout(() => {
+      parrafoAlerta.style.opacity = 1;
+    }, 200);
+    setTimeout(() => {
+      parrafoAlerta.style.opacity = 0;
+      parrafoAlerta.style.marginBottom = `70px`
+      
+    }, 3600);
+    setTimeout(() => {
+      parrafoAlerta.remove()
+    }, 6000);
+  } else{
+    parrafoAlerta.classList.add(`div__ErrorAlert`,`transition`);
+    parrafoAlerta.innerHTML = `<p class="p__ErrorAlert">¡Error, la contraseña es incorrecta!</p>`;
+    divIniciarSesion.appendChild(parrafoAlerta)
+    setTimeout(() => {
+      parrafoAlerta.style.opacity = 1;
+    }, 200);
+    setTimeout(() => {
+      parrafoAlerta.style.opacity = 0;
+      parrafoAlerta.style.marginBottom = `70px`
+      
+    }, 3600);
+    setTimeout(() => {
+      parrafoAlerta.remove()
+    }, 6000);
   }
 });
